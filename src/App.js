c7ebd161746e337f59c752545373e188791683ed
kunal87raman <kunal87raman@gmail.com>
2020-10-05 00:01:10 +0530

    Committing the changes to have git add -u! again

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   const trainer="Srinivas Dande Sir";
   return (
     <div className="myclass">
-      <h2>Welcome to {title} !</h2>
-      <h3>You are learning ReactJS 16 from {trainer} !</h3>
+      <h2>Welcome to {title} !!!</h2>
+      <h3>You are learning ReactJS 16 from {trainer} !!!</h3>
     </div>
   );
 }
